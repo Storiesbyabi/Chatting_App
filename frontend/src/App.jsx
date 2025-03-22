a11fab15588411d0bae57d128b7b17c33345bc90
Storiesbyabi <sabhijith595@gmail.com>
2025-03-22 20:10:41 +0530

    theme issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,14 @@ import { useAuthStore } from './store/useAuthStore'
 import { useEffect } from 'react'
 import {Loader} from 'lucide-react'
 import {Toaster} from 'react-hot-toast'
+import { useThemeStore } from './store/useThemeStore'
 
 
 const App = () => {
 
   const {authUser,checkAuth,isCheckingAuth} = useAuthStore()
+
+  const {theme}=useThemeStore()
 
   useEffect(()=>{
     checkAuth()
@@ -30,7 +33,7 @@ const App = () => {
   
 
   return (
-    <div >
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route path='/' element={authUser? <HomePage/>: <Navigate to='/login'/>}/>
